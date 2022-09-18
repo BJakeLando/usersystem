19576225d72ca4a9e5f8b7477e749d00c41143bc
BJakeLando <bjlando@gmail.com>
2022-09-18 09:32:01 -0700

    FSDI 105 Competency Report
    This the registration page with a login

--- scripts/register.js
+++ scripts/register.js
@@ -1,9 +1,9 @@
 // add payment method using a select element on the html, add it to the constructor
 
 //objejct constructor
-function User(email,pasword,firstName,lastName,age, gender,address, phone,color,payment){
+function User(email,password,firstName,lastName,age, gender,address, phone,color,payment){
     this.email=email;
-    this.password=pasword;
+    this.password=password;
     this.firstName=firstName;
     this.lastName=lastName;
     this.age=age;
@@ -20,15 +20,63 @@ function User(email,pasword,firstName,lastName,age, gender,address, phone,color,
 
 function isValid(aUser){
     let valid = true;
+    $("input").removeClass("input-error");
 
     if(aUser.email == "" || aUser.password=="" || aUser.name == "" ){
         valid=false;
-        alert("Please enter an email");
+        $("#userEmail").addClass("input-error");
+        console.log("Please enter a user email");
+    }
+    if(aUser.firstName==""){
+        valid=false;
+        $("#userFirstName").addClass("input-error");
+        console.log("Please add a user name");
+
+    }
+    if(aUser.lastName==""){
+        valid=false;
+        $("#userLastName").addClass("input-error");
+        console.log("Please add a user name");
+
+    }
+    if(aUser.phone==""){
+        valid=false;
+        $("#userPhone").addClass("input-error");
+        console.log("Please add a user name");
+
+    }
+    if(aUser.password.length==0){
+        valid=false;
+        $("#userPassword").addClass("input-error");
     }
 
     return valid;
 
 }
+function validatePass(){
+    console.log("Validating password");
+    let inputPass=$("#userPassword");//getting the input #userPassword
+    let password = inputPass.val(); //getting the value
+    if(password.length<6){
+        inputPass.css("background-color", "#ff9898");
+        console.log("The password is too short"); 
+        displayErrors("The password is too short");       
+    }else{
+        inputPass.css("background-color", "#008000");
+        console.log("The password has the correct length");
+        hideErrors();
+
+    }
+
+}
+function displayErrors(msg){
+        $("#alert-error").removeClass("hide").text(msg);
+       
+    }
+
+    function hideErrors(){
+        $("#alert-error").addClass("hide");
+    }
 
 function register() {
     let txtFirstName =$("#userFirstName").val();
@@ -48,10 +96,13 @@ function register() {
     let aUser = new User(txtEmail,txtPassword,txtFirstName,txtLastName,txtAge,txtGender,txtAddress,txtPhone,selColor,selPayment);
 
     if(isValid(aUser)){
+        hideErrors();
     saveUser(aUser);
     //clear the inputs 
     $("input").val("");
-}
+    }else{
+        displayErrors("Please complete all the required fields.");
+    }
 }
 
 
@@ -76,6 +127,8 @@ function init(){
         $(".form-container").slideUp(1000);
     
     });
+
+    $("#userPassword").keyup(validatePass);
 
 
 }
